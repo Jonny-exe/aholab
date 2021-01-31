c86abc48eda04f9f6b94e211f41758f52ae448be
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-01-31 15:27:05 +0100

    Adde new build with different name

---
+++ back/mintzai-httpd/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/Jonny-exe/aholab/back/mintzai-httpd/handlers"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+func handleRequest() error {
+	// err := sql.Register("mysql", &MySQLDriver{})
+	// if err != nil {
+	// 	log.Println("Error: Could NOT connect to database.")
+	// 	log.Println(err)
+	// 	return err
+	// }
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/InsertUserInfo", handlers.InsertUserInfo).Methods("POST", "OPTIONS")
+	myRouter.HandleFunc("/GetAudioFileAmount", handlers.GetAudioFileAmount).Methods("GET", "OPTIONS")
+	myRouter.HandleFunc("/", handlers.Test).Methods("GET", "OPTIONS")
+	acceptedOrigins := os.Getenv("AHOLAB_ACCEPTED_ORIGINS")
+	log.Println(acceptedOrigins)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{acceptedOrigins},
+		AllowCredentials: false,
+		AllowedMethods:   []string{"POST", "GET", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+
+		// Enable Debugging for testing, consider disabling in production
+		// To debug turn this to true
+		Debug: false,
+	})
+
+	PORT := 8080
+	corsHandler := c.Handler(myRouter)
+	log.Println("Listening on port: ", PORT)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(PORT), corsHandler))
+
+	return nil
+}
+
+func main() {
+	err := handleRequest()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
