574f651a4f79f32e3bdba98b183ed27b2de85f73
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-02-02 18:34:35 +0100

    new handler and server

--- back/mintzai-httpd/main.go
+++ back/mintzai-httpd/main.go
@@ -6,34 +6,35 @@ import (
 	"strconv"
 
 	"github.com/Jonny-exe/aholab/back/mintzai-httpd/handlers"
-	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
 
 func handleRequest() error {
 	// myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter := mux.NewRouter()
-	myRouter.HandleFunc("/InsertUserInfo", handlers.InsertUserInfo).Methods("POST", "OPTIONS")
-	myRouter.HandleFunc("/GetAudioFileAmount", handlers.GetAudioFileAmount).Methods("GET", "OPTIONS")
-	myRouter.HandleFunc("/", handlers.Test).Methods("GET", "OPTIONS")
-	acceptedOrigins := "https://aholab.ehu.eus/users/aitor"
-	log.Println(acceptedOrigins)
-	c := cors.New(cors.Options{
-		//AllowedOrigins:   []string{acceptedOrigins},
-		AllowedOrigins:   []string{"*"},
-		AllowCredentials: false,
-		AllowedMethods:   []string{"POST", "GET", "OPTIONS"},
-		AllowedHeaders:   []string{"*"},
+	// myRouter := http.NewServeMux()
+	// myRouter.HandleFunc("/InsertUserInfo", handlers.InsertUserInfo).Methods("POST", "OPTIONS")
+	// myRouter.HandleFunc("/GetAudioFileAmount", handlers.GetAudioFileAmount).Methods("GET", "OPTIONS")
+	// acceptedOrigins := "https://aholab.ehu.eus/users/aitor"
+	// log.Println(acceptedOrigins)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.Test)
+	// c := cors.New(cors.Options{
+	// 	//AllowedOrigins:   []string{acceptedOrigins},
+	// 	AllowedOrigins:   []string{"*"},
+	// 	AllowCredentials: false,
+	// 	AllowedMethods:   []string{"POST", "GET", "OPTIONS"},
+	// 	AllowedHeaders:   []string{"*"},
 
-		// Enable Debugging for testing, consider disabling in production
-		// To debug turn this to true
-		Debug: true,
-	})
+	// 	// Enable Debugging for testing, consider disabling in production
+	// 	// To debug turn this to true
+	// 	Debug: true,
+	// })
 
 	PORT := 8080
-	corsHandler := c.Handler(myRouter)
+	// corsHandler := c.Handler(myRouter)
+	handler := cors.Default().Handler(mux)
 	log.Println("Listening on port: ", PORT)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(PORT), corsHandler))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(PORT), handler))
 
 	return nil
 }
